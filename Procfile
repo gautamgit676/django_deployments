b2fceaa2151f7a598cf74936501b7b297cf382d5
gautamgit676 <gautamgit244@gmail.com>
2025-12-29 20:22:26 +0530

    views add

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn pro.wsgi  --log-file -
+web: gunicorn pro.wsgi  --log-file -web:
+ 
--- pro/settings.py
+++ pro/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = "django-insecure-*@0a@10jn)aeqtj1@)@kx0t68sx^1qjrx0#1^h%n4^6sz-pd^_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['"app.up.railway.app"']
-CSRF_TRUSTED_ORIGINS = ['https://app.up.railway.app/']
+ALLOWED_HOSTS = ['"djangod-production.up.railway.app"']
+CSRF_TRUSTED_ORIGINS = ['https://djangod-production.up.railway.app/']
 
 
 # Application definition
