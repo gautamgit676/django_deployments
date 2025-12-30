56d3005c28f92b83122ef6ba6e7364c31bfba735
gautamgit676 <gautamgit244@gmail.com>
2025-12-30 20:59:53 +0530

    asdd cod

--- pro/settings.py
+++ pro/settings.py
@@ -24,8 +24,8 @@ SECRET_KEY = "django-insecure-*@0a@10jn)aeqtj1@)@kx0t68sx^1qjrx0#1^h%n4^6sz-pd^_
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ["djangod-production.up.railway.app",'localhost:8000']
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["djangod-production.up.railway.app"]
 CSRF_TRUSTED_ORIGINS = ['https://djangod-production.up.railway.app/']
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
